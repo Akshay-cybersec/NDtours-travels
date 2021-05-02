6b92d537df22b665cfafd642501be3941e243723
Akshay Jadhav <83538162+Akshay-cybersec@users.noreply.github.com>
2021-05-02 17:08:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+header = document.querySelector('.header')
+navbar = document.querySelector('.navbar')
+logo = document.querySelector('.logo')
+
+burger.addEventListener('click' , ()=>{
+    header.classList.toggle('h-res')
+    navbar.classList.toggle('vclass')
+    logo.classList.toggle('vclass')
+})
+
+alert("This website is just for project purpose not for commercial")
